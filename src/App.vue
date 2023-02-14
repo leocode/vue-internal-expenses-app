f2fbebd97466c6f3afb2228decc41d957450aa10
Mateusz Pigula <pigula.mateusz@gmail.com>
2023-02-14 15:48:57 +0100

    feat: basic app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+import Navigation from './components/Navigation.vue';
+
+<template>
+  <div id="app">
+    <header>
+      <Navigation />
+    </header>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container mx-auto mb-8">
+    <nav class="flex gap-8 py-4">
+      <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
+    </nav>
+  </div>
+</template>
