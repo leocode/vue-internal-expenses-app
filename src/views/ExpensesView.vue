33e8846473b8d68a996a3dbf51a0108d48398a79
Mateusz Pigula <pigula.mateusz@gmail.com>
2023-02-14 16:08:33 +0100

    feat: implement routing

---
+++ src/views/ExpensesView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="container mx-auto">
+    <h1>This is expenses view page.</h1>
+  </main>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,3 @@
+<template>
+  <main class="container mx-auto">This is login page.</main>
+</template>
