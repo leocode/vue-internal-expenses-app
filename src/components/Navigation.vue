74e8e2aefca7acc9fccfb6d50fe7b62f0c63d75b
Mateusz Pigula <pigula.mateusz@gmail.com>
2023-03-05 19:50:48 +0100

    feat: navigation menu items

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,8 +1,31 @@
 <template>
   <div class="container mx-auto mb-8">
     <nav class="flex gap-8 py-4">
-      <router-link to="/">Home</router-link>
-      <router-link to="/login">Login</router-link>
+      <router-link v-for="link in menuItems" :to="link.url">{{
+        link.label
+      }}</router-link>
     </nav>
   </div>
 </template>
+
+<script>
+const loggedInMenu = [
+  { url: "/", label: "Home" },
+  { url: "/settings", label: "Settings" },
+];
+
+const loggedOutMenu = [{ url: "/login", label: "Login" }];
+
+export default {
+  data() {
+    return {
+      isAuthorized: true, // TODO: handle login state
+    };
+  },
+  computed: {
+    menuItems: function () {
+      return this.isAuthorized ? loggedInMenu : loggedOutMenu;
+    },
+  },
+};
+</script>
